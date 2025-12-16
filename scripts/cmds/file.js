00888e09e703bc149a6d01cf85566da29cb12091
tamimbbz <tanishajannat721615@gmail.com>
2025-12-16 12:21:16 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 module.exports = {
 	config: {
 		name: "file",
-		aliases: ["files", "sendfile"],
+		aliases: ["file", "sendfile"],
 		version: "1.0",
-		author: "404",
+		author: "Tamu",
 		countDown: 5,
 		role: 0,
 		shortDescription: "Send bot script",
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100045644423035"];
+		const permission = ["100087466441450"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("📛 You have no permission this cmd only  used this cmd..", event.threadID, event.messageID);
 		}
